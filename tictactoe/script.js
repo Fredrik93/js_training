85b59912cf33e51159534f0a4da28b6414808cd4
Fredrik93 <fredrikullman93@gmail.com>
2020-07-28 17:15:22 +0200

    add grid

---
+++ tictactoe/script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cardArray = [
+        {
+            name: 'drawer',
+            img: 'images/drawer.png'
+        }, {
+            name: 'head',
+            img: 'images/head.png'
+        }, {
+            name: 'shield',
+            img: 'images/shield.png'
+        }, {
+            name: 'tree',
+            img: 'images/tree.png'
+        }, {
+            name: 'tv',
+            img: 'images/tv.png'
+        }, {
+            name: 'wordpress',
+            img: 'images/wordpress.png'
+
+        }, {
+            name: 'wrench',
+            img: 'images/wrench.png'
+        }
+    ]
+
+
+
+
+
+
+
+
+})
